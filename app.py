13e49d4942a92650fc712b9231627903f2385057
Dominick DiLeo <nickkcindi@outlook.com>
2022-03-31 16:10:46 -0400

    version2 commit. Copied new function from version1 and put it on same lines. And added another new funtion after that

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 def func1():
     print("this is function 1")
+
+
+def func2():
+    print("this is function 2. Made in version 1")
+
+
+def func3():
+    print("this is function 3. Made in version 2")
